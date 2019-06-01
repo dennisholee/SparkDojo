f491158f3d77c04545944432984ecef083bb0e8a
dennisholee@gmail.com <dennisholee@gmail.com>
2019-06-01 19:28:57 +0800

    Fix feature bug and added visualization

--- ml_salary_prediction/ml_salary_prediction.py
+++ ml_salary_prediction/ml_salary_prediction.py
@@ -3,30 +3,28 @@ from pyspark import SparkContext
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.feature import VectorAssembler
 
-import matplotlib.pyplot as plt
-
-#salarydata_file = '/home/dennis/Dev/SPARK/SparkDojo/rddcount/Salary_Data.csv'
+# Assumes the file is loaded to HDFS
 salarydata_file = '/user/dennis/ml_salary_prediction/Salary_Data.csv'
 
 spark = SparkSession.builder.master('local').getOrCreate()
 
-sc = spark.sparkContext
-
 salary_dataframe = spark.read.format('CSV').option('header', 'true').option('inferSchema', 'true').load(salarydata_file)
 salary_dataframe.printSchema()
 
-assembler = VectorAssembler(inputCols=['YearsExperience','Salary'], outputCol='features')
+assembler = VectorAssembler(inputCols=['YearsExperience'], outputCol='features')
 
 data = assembler.transform(salary_dataframe)
 
 train, test = data.randomSplit([0.7, 0.3])
+train.printSchema()
 
-algo = LinearRegression(labelCol='Salary')
+algo = LinearRegression(featuresCol='features', labelCol='Salary', maxIter=10, regParam=0.3, elasticNetParam=0.8)
 
 model = algo.fit(train)
 
 # Print the coefficients and intercept for linear regression
 print('---------------------------------')
+
 print('Coefficient: %s' % str(model.coefficients))
 print('Intercept: %s' % str(model.intercept))
 
@@ -43,5 +41,15 @@ predictions = model.transform(test)
 predictions.printSchema()
 predictions.select('YearsExperience', 'Salary', 'prediction').show()
 
-# plt.scatter(train.X, train.Y, color='red')
-# plt.show()
+years = data.select('YearsExperience').collect()
+salary = data.select('Salary').collect()
+
+predictYears = predictions.select('YearsExperience').collect()
+predictSalary = predictions.select('prediction').collect()
+
+import matplotlib.pyplot as plt
+
+plt.scatter(years, salary)
+plt.plot(predictYears, predictSalary) 
+plt.show()
+
